30c8aa5d7cd9df99e0f5202ce82986393da50d4b
badriyahs <badriyahalsalem@gmail.com>
2024-11-30 22:13:49 -0500

    cursors added

--- script.js
+++ script.js
@@ -9,14 +9,3 @@ function toggleEssay(titleElement) {
     const content = titleElement.nextElementSibling;
     content.classList.toggle("expanded");
 }
-document.querySelectorAll('.line-circle').forEach((lineCircle) => {
-    const xValue = parseInt(lineCircle.dataset.x, 10);
-    const maxX = 30; // Maximum x value
-    const circle = lineCircle.querySelector('.circle');
-
-    // Calculate percentage position based on x value
-    const positionPercentage = (xValue / maxX) * 100;
-
-    // Apply the position to the circle
-    circle.style.left = `${positionPercentage}%`;
-});
